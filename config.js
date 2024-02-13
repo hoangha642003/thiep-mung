51d160facba568684207a46c4cf1dca21917f373
HoangHa <129708029+hoangha642003@users.noreply.github.com>
2024-02-13 14:29:45 +0700

    ghhj

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
+const nameGirl = 'Quỳnh Dim';
+const giftUrl = 'https://www.facebook.com/';
+const eventName = 'Chúc Mừng Valentine';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc quỳnh dim một Valentine thật vui vẻ và hãy nhớ đến tớ . Cảm ơn Thượng đế đã cho tớ gặp  một  người con gái đáng yêu xinh đẹp, giỏi giang, hiền lành. Chúc em yêu một ngày Valentine vui vẻ, hạnh phúc. Làm người iu tớ nhé quỳnh dim!';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+
+const giftImage = 'z5157500776822_0b049daff18f527754293a4dc5f509c0.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
