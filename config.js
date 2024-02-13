3a9709b94aaa069226be0528bd7b3d81b4e77f3c
HoangHa <129708029+hoangha642003@users.noreply.github.com>
2024-02-13 17:56:14 +0700

    ghk

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Quỳnh Dim';
-const giftUrl = 'https://www.facebook.com/';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100033441023558';
 const eventName = 'Chúc Mừng Valentine';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc quỳnh dim một Valentine thật vui vẻ và hãy nhớ đến tớ . Cảm ơn Thượng đế đã cho tớ gặp  một  người con gái đáng yêu xinh đẹp, giỏi giang, hiền lành. Chúc em yêu một ngày Valentine vui vẻ, hạnh phúc. Làm người iu tớ nhé quỳnh dim!';
+const contentCard = 'Chúc quỳnh dim một Valentine thật vui vẻ và hãy nhớ đến tớ . Cảm ơn Thượng đế đã cho tớ gặp  một  người con gái đáng iu xinh đẹp,  hiền lành - mãi là cô bé đáng iu trong lòng tớ nhé !. Chúc em yêu một ngày Valentine vui vẻ, hạnh phúc. Làm người iu tớ nhé quỳnh dim!';
 
 
 const giftImage = 'z5157500776822_0b049daff18f527754293a4dc5f509c0.jpg';
